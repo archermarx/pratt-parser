e1fac5ebc20d44ff89b7f09b7ad43098996249ba
Thomas Marks <marksta@umich.edu>
2024-11-08 12:38:09 -0500

    Add unary operators

--- pratt.cpp
+++ pratt.cpp
@@ -17,10 +17,10 @@ using isize = ptrdiff_t;
 using f32 = float;
 using f64 = double;
 
-using std::string, std::vector, std::array, std::cout, std::format;
+using std::string, std::vector, std::array, std::cout, std::endl, std::format;
 using std::unique_ptr, std::make_unique, std::pair;
 
-constexpr i64 powi(i64 x, usize p) {
+constexpr i64 powu(i64 x, usize p) {
   // special casing
   switch(p) {
     case (0): return 1;
@@ -41,22 +41,29 @@ constexpr i64 powi(i64 x, usize p) {
   }
 
   i64 remainder = p - p_cur;
-  return res * powi(x, remainder);
+  return res * powu(x, remainder);
+}
+
+i64 powi(i64 x, i64 p) {
+  if (p < 0) throw std::runtime_error("Integer cannot be raised to negative power");
+  return powu(x, (usize)p);
 }
 
 //== Tokenizer ====={{{
 ////== Op definition ====={{{
 struct Op {
+    // Name, symbol, left infix bp, right infix bp, prefix bp
+    // zero bp means the operator is invalid in that position
 #define OP_LIST \
-    X(Add, +, 1, 2) \
-    X(Sub, -, 1, 2) \
-    X(Mul, *, 3, 4) \
-    X(Div, /, 3, 4) \
-    X(Exp, ^, 5, 6)
+    X(Add, +, 1, 2, 5) \
+    X(Sub, -, 1, 2, 5) \
+    X(Mul, *, 3, 4, 0) \
+    X(Div, /, 3, 4, 0) \
+    X(Exp, ^, 7, 8, 0)
 
   enum class Kind {
     None = 0,
-    #define X(OpKind, _0, _1, _2) OpKind,
+    #define X(OpKind, _0, _1, _2, _3) OpKind,
     OP_LIST
     #undef X
     NumOps,
@@ -65,14 +72,21 @@ struct Op {
   Kind kind = Kind::None;
 
   static constexpr array LeftBindingPowers {
-    #define X(_0, _1, bp, _2) static_cast<u8>(bp),
+    #define X(_0, _1, bp, _2, _3) static_cast<u8>(bp),
     static_cast<u8>(0),
     OP_LIST
     #undef X
   };
 
   static constexpr array RightBindingPowers {
-    #define X(_0, _1, _2, bp) static_cast<u8>(bp),
+    #define X(_0, _1, _2, bp, _3) static_cast<u8>(bp),
+    static_cast<u8>(0),
+    OP_LIST
+    #undef X
+  };
+
+  static constexpr array InfixBindingPowers {
+    #define X(_0, _1, _2, _3, bp) static_cast<u8>(bp),
     static_cast<u8>(0),
     OP_LIST
     #undef X
@@ -84,8 +98,12 @@ struct Op {
     return {l_bp, r_bp};
   }
 
+  u8 prefix_binding_power() const {
+    return this->InfixBindingPowers[static_cast<usize>(this->kind)];
+  }
+
   static constexpr array Names {
-    #define X(OpKind, _0, _1, _2) #OpKind,
+    #define X(OpKind, _0, _1, _2, _3) #OpKind,
     "NoOp",
     OP_LIST
     #undef X
@@ -96,7 +114,7 @@ struct Op {
   }
 
   static constexpr array Symbols {
-    #define X(_0, OpSymbol, _1, _2) #OpSymbol,
+    #define X(_0, OpSymbol, _1, _2, _3) #OpSymbol,
     "<none>",
     OP_LIST
     #undef X
@@ -106,6 +124,14 @@ struct Op {
     return this->Symbols[static_cast<size_t>(this->kind)];
   }
 
+  i64 eval(i64 x)  {
+    switch(this->kind) {
+      case Kind::Add: return x;
+      case Kind::Sub: return -x;
+      default: throw std::runtime_error(format("Invalid unary operator '{}'. This should be unreachable.", this->symbol()));
+    }
+  }
+
   i64 eval(i64 left, i64 right) {
     switch(this->kind){
       case Kind::Add: return left + right;
@@ -113,7 +139,7 @@ struct Op {
       case Kind::Mul: return left * right;
       case Kind::Div: return left / right;
       case Kind::Exp: return powi(left, right);
-      default: throw std::runtime_error(format("Invalid operator {}. This should be unreachable", this->name()));
+      default: throw std::runtime_error(format("Invalid infix operator '{}'. This should be unreachable", this->symbol()));
     }
 
   }
@@ -210,7 +236,7 @@ struct Tokenizer {
 
     u8 c = peek();
 
-    #define X(_Type, _Symbol, _1, _2) case (#_Symbol)[0]: { index++; return Op{Op::Kind::_Type}; }
+    #define X(_Type, _Symbol, _1, _2, _3) case (#_Symbol)[0]: { index++; return Op{Op::Kind::_Type}; }
     switch(c) {
       OP_LIST
       default:
@@ -239,16 +265,21 @@ struct Expr{
   enum class Kind {
     None,
     Literal,
-    Expr,
+    Unary,
+    Binary,
   } kind = kind = Kind::None;
 
   union {
     i64 literal;
     struct {
       Op op;
+      unique_ptr<Expr> expr;
+    } unary;
+    struct {
+      Op op;
       unique_ptr<Expr> left;
       unique_ptr<Expr> right;
-    } expr;
+    } binary;
   };
 
   string str();
@@ -257,20 +288,25 @@ struct Expr{
 
   Expr(): kind(Kind::None) {};
   Expr(i64 val): kind(Kind::Literal), literal(val) {}
-  Expr(Op op, unique_ptr<Expr> left, unique_ptr<Expr> right) {
-    kind = Kind::Expr;
-    expr.op = {op};
-    new (&expr.left) unique_ptr<Expr>(std::move(left));
-    new (&expr.right) unique_ptr<Expr>(std::move(right));
+  Expr(Op op, unique_ptr<Expr> expr): kind(Kind::Unary) {
+    unary.op = op;
+    new (&unary.expr) unique_ptr<Expr>(std::move(expr));
+  }
+  Expr(Op op, unique_ptr<Expr> left, unique_ptr<Expr> right): kind(Kind::Binary) {
+    binary.op = {op};
+    new (&binary.left) unique_ptr<Expr>(std::move(left));
+    new (&binary.right) unique_ptr<Expr>(std::move(right));
   }
   ~Expr() {
     switch(kind) {
       case Kind::None:
       case Kind::Literal:
         break;
-      case Kind::Expr:
-        expr.left.~unique_ptr<Expr>();
-        expr.right.~unique_ptr<Expr>();
+      case Kind::Unary:
+        unary.expr.~unique_ptr<Expr>();
+      case Kind::Binary:
+        binary.left.~unique_ptr<Expr>();
+        binary.right.~unique_ptr<Expr>();
         break;
     }
   }
@@ -283,7 +319,8 @@ struct Expr{
     switch(this->kind) {
       case Kind::None: throw std::runtime_error("Attempt to eval expr of type None");
       case Kind::Literal: return this->literal;
-      case Kind::Expr: return this->expr.op.eval(this->expr.left->eval(), this->expr.right->eval());
+      case Kind::Unary:   return this->unary.op.eval(this->unary.expr->eval());
+      case Kind::Binary:  return this->binary.op.eval(this->binary.left->eval(), this->binary.right->eval());
     } 
   }
 };
@@ -292,7 +329,8 @@ string Expr::str() {
   switch(kind) {
     case Kind::None: return "<none>";
     case Kind::Literal: return format("{}", literal);
-    case Kind::Expr: return format("({} {} {})", expr.op.symbol(), expr.left->str(), expr.right->str());
+    case Kind::Unary: return format("({} {})", unary.op.symbol(), unary.expr->str(), literal);
+    case Kind::Binary: return format("({} {} {})", binary.op.symbol(), binary.left->str(), binary.right->str());
   }
 }
 ////== end expr struct definition }}}
@@ -312,21 +350,28 @@ struct Parser {
     return tok;
   }
 
-  Token expect(Token::Kind expected) {
-    auto got = this->next();
-    if (got.kind != expected) {
-      throw std::runtime_error(
-        format("Expected token of kind {}, got {}", 
-               tokenkind_name(expected), tokenkind_name(got.kind))
-      );
-    }
-    return got;
-  }
-  
   unique_ptr<Expr> parse_expr(u8 min_bp = 0) {
-    auto lhs_val = this->expect(Token::Kind::Int);
-    auto lhs = Expr::Literal(lhs_val.integer);
+    auto lhs_tok = this->next();
+    //cout << lhs_tok.str() << endl;
+    unique_ptr<Expr> lhs;
+    switch (lhs_tok.kind) {
+      case Token::Kind::Int:{
+        lhs = Expr::Literal(lhs_tok.integer);
+        break;
+      }
+      case Token::Kind::Op: {  
+        // prefix operator
+        auto op = lhs_tok.op;
+        auto bp = op.prefix_binding_power();
+        if (bp == 0) throw std::runtime_error(format("Invalid unary operator '{}'", op.symbol()));
+        auto rhs = parse_expr(bp);
+        lhs = make_unique<Expr>(op, std::move(rhs));
+        break;
+      }
+      default: throw std::runtime_error(format("Expected operator or number, got '{}'", lhs_tok.str()));
+    }
 
+    // parse binary operators
     while (true) {
       auto tok = peek();
       if (tok.kind == Token::Kind::None) break; // EOF
@@ -351,24 +396,18 @@ struct Parser {
 //== end parser }}}
 
 int main(int argc, char **argv) {
+  // TODO: too much validation at all stages, leading to exceptions peppered throughout
+  // possibly remove things like None from many stages
+  // also, return errors instead of panicking, perhaps
+  
   if (argc == 1) throw std::runtime_error("No argument!");
 
   string stream = "";
-  bool print_tokens = false;
-  bool print_ast = false;
+  bool print_tokens = true;
+  bool print_ast = true;
 
   for (int i = 1; i < argc; i++) {
     string arg = argv[i];
-    if (arg[0] == '-') {
-      for (auto c: arg) {
-        switch(c) {
-          case 'a': print_ast = true; break;
-          case 't': print_tokens = true; break;
-          default: break;
-        }
-      }
-      continue;
-    }
     if (stream != "") {
       throw std::runtime_error("Too many arguments");
     }
@@ -377,8 +416,6 @@ int main(int argc, char **argv) {
 
   Tokenizer tokenizer(stream);
   auto tokens = tokenizer.tokenize();
-
-
 
   if (print_tokens) {
     cout << "#== Tokens ==\n";
